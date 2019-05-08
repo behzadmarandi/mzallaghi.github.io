2c896602e554f6c927ee9d9e1a5bef2008caff98
Mehdi Zallaghi <mmzallaghi@gmail.com>
2019-05-08 10:12:02 +0430

    Create fjava.js

---
+++ fjava.js
@@ -0,0 +1,14 @@
+(function(global){
+  var module = global.noise = {};
+
+  function Grad(x, y, z) {
+    this.x = x; this.y = y; this.z = z;
+  }
+  
+  Grad.prototype.dot2 = function(x, y) {
+    return this.x*x + this.y*y;
+  };
+
+  Grad.prototype.dot3 = function(x, y, z) {
+    return this.x*x + this.y*y + this.z*z;
+  };
